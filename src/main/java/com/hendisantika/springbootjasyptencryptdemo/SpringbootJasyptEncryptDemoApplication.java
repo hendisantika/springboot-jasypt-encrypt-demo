817e6d3c84036eb5df606fae4afc7dbf8c5aafba
Hendi Santika <hendisantika@gmail.com>
2018-08-09 05:29:54 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootjasyptencryptdemo/SpringbootJasyptEncryptDemoApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.springbootjasyptencryptdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJasyptEncryptDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJasyptEncryptDemoApplication.class, args);
+    }
+}
